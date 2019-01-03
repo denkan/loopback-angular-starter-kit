884f846957dbec27c6d267510b57487f60e31d83
Dennis Nyström <dennis.nystrom@gmail.com>
2019-01-03 22:21:09 +0100

    [BE] clean up tests, which barely exists anyway (I suck...)

--- backend/tests/game-user.spec.ts
+++
@@ -1,9 +0,0 @@
-var should    = require('chai').should();
-var supertest = require('supertest');
-var api       = supertest('http://localhost:3000/api');
-
-describe('GameUser unit tests:', () => {
-    it('Should create a GameUser instance', (done: Function) => {
-        api.post('/game-users').send({}).expect(200, done);
-    });
-});
--- backend/tests/source-xml-soccer.spec.ts
+++
@@ -1,9 +0,0 @@
-var should    = require('chai').should();
-var supertest = require('supertest');
-var api       = supertest('http://localhost:3000/api');
-
-describe('SourceXMLSoccer unit tests:', () => {
-    it('Should create a SourceXMLSoccer instance', (done: Function) => {
-        api.post('/source-xml-soccers').send({}).expect(200, done);
-    });
-});
